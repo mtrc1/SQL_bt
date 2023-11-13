b87749284e4d2b8984516e0a71da2a46e572f0bb
mtrc1 <150705420+mtrc1@users.noreply.github.com>
2023-11-14 02:32:05 +0900

    Create day3_bt.sql

---
+++ day3_bt.sql
@@ -0,0 +1,23 @@
+__bt1
+  select*from CITY where population >=100000 and CountryCode ="usa"
+__bt2
+Select*from City where countrycode="jpn"
+__bt3
+select City, State from station
+__bt4
+select city 
+from station 
+where city like "a%" or city like "i%" or city like "u%" or city like "e%" or city like "o%"
+__bt5
+select distinct city 
+from station 
+where city like "%a"or city like "%i" or city like "%u" or city like "%e" or city like "%o"
+__bt6
+select distinct city 
+from  station
+where city not like "a%"
+And city not like "i%"
+And city not like "u%"
+And city not like "e%"
+And city not like "o%"
+__bt7
