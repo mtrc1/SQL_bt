858f2e0d97f6241b72793ef5e4b2e17a5cc2bd69
mtrc1 <150705420+mtrc1@users.noreply.github.com>
2023-11-17 11:17:52 +0900

    Update bt_buoi_5.sql

--- bt_buoi_5.sql
+++ bt_buoi_5.sql
@@ -21,4 +21,10 @@ FROM posts
 where post_date>= '2021-01-01'and post_date < '2022-01-01'
 group by user_id 
 having count(*)>=2;
+__bt7
+SELECT card_name, (max(issued_amount)-min(issued_amount)) AS difference
+FROM monthly_cards_issued
+group by card_name
+order by difference desc;
+__bt8
 
