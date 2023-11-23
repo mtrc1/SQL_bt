2600a168840c45534a2f987a37982a3804bf9c81
mtrc1 <150705420+mtrc1@users.noreply.github.com>
2023-11-23 16:41:37 +0900

    Update bt_day9.sql

--- bt_day9.sql
+++ bt_day9.sql
@@ -11,4 +11,17 @@ when z+x<=y then 'no'
 else 'yes' end as triangle
 from triangle
 __ex3
-
+select ROUND(1.0* sum(case when call_category is null or call_category = 'n/a' 
+then 1 else 0 end)/ count(*)*100,1) 
+as call_percentage
+from callers
+__ex4
+select name from customer
+where referee_id<> "2" or referee_id is null
+__ex5
+select survived,
+sum(case when pclass = 1 then 1 end) first_class,
+sum(case when pclass = 2 then 1 end) second_class,
+sum(case when pclass = 3 then 1 end) third_class
+from titanic
+group by survived
